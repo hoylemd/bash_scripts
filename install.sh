df2c9fa044e050c1bd561ccbfd642b5d79100a91
Michael D. Hoyle <hoylemd@gmail.com>
2015-04-13 23:27:43 -0400

    improve install script

--- install.sh
+++ install.sh
@@ -1,7 +1,31 @@
 #! /bin/bash
 
-target=$1
+loc=$(pwd)
+cd $1
 
-ln update.sh $target/
-ln rebase.sh $target/
-ln fg.sh $target/
+# script installer/updater
+# params: source, destination
+install () {
+  echo installing $1 to $2
+  ln -fs $1 $2
+
+  return 0
+}
+
+for script in "update.sh" "rebase.sh" "fg.sh"
+do
+  echo $loc
+  target="$loc/$script"
+  install $target $script
+done
+
+if [ ! -e .fgrc ]; then
+  cp $loc/.fgrc .
+fi
+
+for script in "update.sh" "rebase.sh" "fg.sh" ".fgrc" "my.*"
+do
+  echo $script >> .git/info/exclude
+done
+
+vim .fgrc
--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 git co master
 git fetch origin
 git merge mhoyle/master
-git push origin
+git push mhoyle
 git co $branch
