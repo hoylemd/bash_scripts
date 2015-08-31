d932a65215a236155863ef483784442e7b3b23c8
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-08-31 15:32:53 -0400

    add applytemplate file

---
+++ applytemplate.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Render a Stemp template.')
+
+help_string = 'Path to the values file. Should be in json dictionary format'
+parser.add_argument('values_path', help=help_string)
+
+help_string = 'Path to the input template. Stdin Will be used if omitted.'
+parser.add_argument('-i', '-t', '--input-template', dest='template_path',
+                    help=help_string)
+
+help_string = 'Path to the output file. Stdout will be used if omitted'
+parser.add_argument('-o', '--output-path', dest='output_path',
+                    help=help_string)
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    print 'values at: %s' % args.values_path
+    print 'input at: %s' % (args.template_path or 'stdin')
+    print 'values at: %s' % (args.output_path or 'stdout')
