27a0a3878b12afc9430894d42c270a9dece92eb2
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-01 19:42:17 -0400

    add some ssh agent stuff to update

--- tail.sh
+++ tail.sh
@@ -1,9 +1,11 @@
 LOGPATH=""
+echo "$LOGPATH"
 source .fgrc
+echo "$LOGPATH"
 
 if [ -e "$LOGPATH" ]; then
   tail -f $LOGPATH
 else
-  echo 'No log to tail :(. Please add it to LOGFILE in .fgrc.'
+  echo 'No log to tail :(. Please add it to LOGPATH in .fgrc.'
 fi
 
--- update.sh
+++ update.sh
@@ -9,15 +9,36 @@ if [ -z "$MYREMOTE" ]; then
   MYREMOTE=mhoyle
 fi
 
-git co master
+git co -q master
+
+# make sure all keys are added
+keys=$(ls ~/.ssh | grep -e ".*_rsa$")
+ssh_list=$(ssh-add -l)
+result=$?
+if [ $result -gt 0 ]; then
+  echo "ssh-agent is not running. Please start it now."
+  exit 1
+else
+  added=$(echo "$ssh_list" | sed -rn 's/.*\.ssh\/([a-zA-Z0-9_-]+) \(RSA\)/\1/p')
+fi
+
+me=$(whoami)
+for key in $keys; do
+  # check if this key exists
+  if [ ! $(echo "$added" | grep $key) ]; then
+    echo "rsa key $key is missing from agent adding it."
+    ssh-add "/home/$me/.ssh/$key"
+  fi
+done
+
 git fetch origin
 if [ $? -gt 0 ]; then
-  exit $?
+  exit 2
 fi
 
 git merge origin/master
 if [ $? -gt 0 ]; then
-  exit $?
+  exit 3
 fi
 
 ./push-upstream.sh
