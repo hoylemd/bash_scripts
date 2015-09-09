ad158c41f00f93eae41cc3a922fa1ef6dc64c892
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-08 21:12:25 -0400

    split into install and create project

---
+++ create_project.sh
@@ -0,0 +1,104 @@
+#! /bin/bash
+
+##
+# Installer/updater script for my dev tools
+#
+# usage: ./install.sh <target directory>
+#
+# if <target directory doesn't exist, it will be created>
+#
+##
+
+# set up a repo
+# params: targetdir
+set_up_repo () {
+  targetdir=$1
+  if [ ! -d $targetdir ]; then
+    mkdir $targetdir
+  fi
+  cd $targetdir
+  echo "setting up repo"
+  git init
+  echo "readme" > README.md
+  git add README.md
+  git commit -m "initial commit"
+}
+
+# make a json key-value pair
+# params: key, value
+json_key_value () {
+  key="$1"
+  value="$2"
+  string="\"$key\": \"$value\""
+  echo "$string"
+}
+
+loc=$(pwd)
+targetdir=$1
+project_name=$2
+FALSE=""
+TRUE="TRUE"
+
+if [ -z "$project_name" ]; then
+  project_name=$(basename $targetdir)
+fi
+
+# get the directory set up
+if [ -d $targetdir ]; then
+  echo "$targetdir already exist, skipping creation"
+else
+  echo "no directory"
+
+  if [ ! -e "~/.fgrc" ]; then
+    cp ~/bash_scripts/.fgrc ~/.fgrc
+  fi
+
+  source ~/.fgrc
+
+  # check connectivity
+  ping -o -t 3 -q $GITHUB_URL
+
+  # if we have internet connection
+  if [ $? -eq 0 ]; then
+    echo "cloning"
+    git clone git@$GITHUB_URL:$MYREMOTE/$project_name $targetdir
+    # if remote repo doesn't exist
+    if [ $? -gt 0 ]; then
+      set_up_repo $targetdir
+      ~/bash_scripts/createRepo.sh $project_name
+      ./push-upstream.sh
+    fi
+  # set it up offline
+  else
+    echo "set up offline"
+    set_up_repo $targetdir
+  fi
+
+  cd $targetdir
+  cp $loc/.fgrc .
+
+  # check that stemp is available
+  if [ ! -e ~/stemp/stemp.py ]; then
+    cd ~
+    git clone git@$GITHUB_URL:$MYREMOTE/Stemp stemp
+  fi
+
+  if [ -e ~/stemp/stemp.py ]; then
+    # ready the values!
+
+    if [ -z "$project_name" ]; then
+      PWD=$(pwd)
+      project_name=$(basename $PWD)
+    fi
+
+    remote_value=$(json_key_value "MYREMOTE" "hoylemd")
+    github_value=$(json_key_value "GITHUB_URL" "github.com")
+    repo_value=$(json_key_value "REPO_NAME" "$project_name")
+    values="{$remote_value, $github_value, $repo_value}"
+    echo $values > /tmp/fgrc_values
+    python ~/stemp/stemp.py /tmp/fgrc_values -i $loc/fgrc.stemp -o .fgrc
+  fi
+fi
+
+#cd $loc
+#./install.sh $targetdir
--- install.sh
+++ install.sh
@@ -1,19 +1,7 @@
 #! /bin/bash
 
-##
-# Installer/updater script for my dev tools
-#
-# usage: ./install.sh <target directory>
-#
-# if <target directory doesn't exist, it will be created>
-#
-##
-
 loc=$(pwd)
-targetdir=$1
-project_name=$2
-FALSE=""
-TRUE="TRUE"
+cd $1
 
 # script installer/updater
 # params: source, destination
@@ -24,47 +12,17 @@ install () {
   ln -fs $src $dest
 }
 
-# git exclusion adder
-# params: exclusion line to add
 add_git_exclusion () {
-  exclusion=$1 # exclusion line to add
+  exclusion=$1 # path to exclude
 
-  # check if the line already exists
   if ! grep -q $exclusion .git/info/exclude;
   then
-    # append the line
     echo $exclusion >> .git/info/exclude
   fi
 }
 
-# set up a repo
-set_up_repo () {
-  echo "setting up repo"
-  git init
-  echo "readme" > README.md
-  git add README.md
-  git commit -m "initial commit"
-}
-
-# make a json key-value pair
-# params: key, value
-json_key_value () {
-  key="$1"
-  value="$2"
-  string="\"$key\": \"$value\""
-  echo "$string"
-}
-
-# make/cd into the target
-setup=$FALSE
-if [ ! -d $targetdir ]; then
-  setup=$TRUE
-  mkdir $targetdir
-fi
-cd $targetdir
-
-scripts="push-upstream.sh update.sh fg.sh go.sh runtests.sh addRemote.sh"
-scripts="$scripts bicep.sh restart.sh tail.sh bg.sh prod.sh"
+scripts="push-upstream.sh update.sh fg.sh go.sh runtests.sh add_remote.sh"
+scripts="$scripts bicep.sh restart.sh tail.sh bg.sh"
 renamed_scripts="install.sh"
 all_scripts="$scripts $renamed_scripts"
 
@@ -80,70 +38,7 @@ for script in $renamed_scripts; do
   install $target $script
 done
 
-if [ ! -e .fgrc ]; then
-  cp $loc/.fgrc .
-  # check that stemp is available
-  if [ ! -e ~/stemp/stemp.py ]; then
-    cd ~
-    git clone git@$GITHUB_URL:$MYREMOTE/Stemp stemp
-  fi
-
-  if [ -e ~/stemp/stemp.py ]; then
-    # ready the values!
-
-    if [ -z "$project_name" ]; then
-      PWD=$(pwd)
-      project_name=$(basename $PWD)
-    fi
-
-    remote_value=$(json_key_value "MYREMOTE" "hoylemd")
-    github_value=$(json_key_value "GITHUB_URL" "github.com")
-    repo_value=$(json_key_value "REPO_NAME" "$project_name")
-    values="{$remote_value, $github_value, $repo_value}"
-    echo $values > /tmp/fgrc_values
-    python ~/stemp/stemp.py /tmp/fgrc_values -i $loc/fgrc.stemp -o .fgrc
-  fi
-  vim .fgrc
-fi
-source .fgrc
-
-code=0
-if [ "$MYREMOTE" ] && [ "$REPO_NAME" ] && [ "$GITHUB_URL" ]; then
-  # check connectivity
-  ping -o -t 3 -q $GITHUB_URL
-  ping_code=$?
-
-  if [ -z "$project_name" ]; then
-    project_name=$REPO_NAME
-  fi
-
-  # if not in a git repo
-  if [ ! -d ".git" ]; then
-    echo "no repo"
-    # if we have internet connection
-    if [ $ping_code -eq 0 ]; then
-      echo "cloning"
-      git clone git@$GITHUB_URL:$MYREMOTE/$REPO_NAME.git
-      # if remote repo doesn't exist
-      if [ $? -gt 0 ]; then
-        set_up_repo
-        ~/bash_scripts/createRepo.sh $project_name
-        ./push-upstream.sh
-      fi
-    # set it up offline
-    else
-      echo "set up offline"
-      set_up_repo
-    fi
-  fi
-# if there isn't remote info
-else
-  git status -s
-  # if no repo
-  if [ $? -gt 0 ]; then
-    set_up_repo
-  fi
-fi
+git init
 
 for script in $all_scripts
 do
@@ -152,11 +47,10 @@ done
 
 add_git_exclusion ".fgrc"
 add_git_exclusion "my.*"
-add_git_exclusion "their.*"
 add_git_exclusion "env"
 
-if [ "$ENV" ]; then
-  if [ ! -d $ENV ]; then
-    virtualenv $ENV
-  fi
+if [ ! -e .fgrc ]; then
+  cp $loc/.fgrc .
+  vim .fgrc
 fi
+
